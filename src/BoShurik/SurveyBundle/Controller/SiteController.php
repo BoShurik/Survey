cbafe8f05a59967fec2b48eb7f6aa9fc3599d3d3
Alexander <boshurik666@mail.ru>
2012-03-03 21:14:03 +0400

    Лишняя строчка -_-

--- src/BoShurik/SurveyBundle/Controller/SiteController.php
+++ src/BoShurik/SurveyBundle/Controller/SiteController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SiteController extends Controller
 {
-    
     public function indexAction()
     {
         return $this->render('BoShurikSurveyBundle:Site:index.html.twig');
