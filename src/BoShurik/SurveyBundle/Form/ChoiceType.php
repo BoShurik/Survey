9548665792a761e60eed7fa8c76f2d47d2167dde
Alexander <boshurik666@mail.ru>
2012-03-03 21:49:45 +0400

    Формы для вопроса и варианта ответа

---
+++ src/BoShurik/SurveyBundle/Form/ChoiceType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BoShurik\SurveyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class ChoiceType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('question')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'boshurik_surveybundle_choicetype';
+    }
+}
---
+++ src/BoShurik/SurveyBundle/Form/QuestionType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BoShurik\SurveyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('expanded')
+            ->add('multiple')
+            ->add('survey')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'boshurik_surveybundle_questiontype';
+    }
+}
