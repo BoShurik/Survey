b1f0c10c897d304111206132146cdb29e29b5e3d
BoShurik <boshurik@gmail.com>
2014-02-24 18:05:38 +0400

    Fix user entity

--- src/BoShurik/UserBundle/Entity/User.php
+++ src/BoShurik/UserBundle/Entity/User.php
@@ -8,10 +8,8 @@
 
 namespace BoShurik\UserBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-
-use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
